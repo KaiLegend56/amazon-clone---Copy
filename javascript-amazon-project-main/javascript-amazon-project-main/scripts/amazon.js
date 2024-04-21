040f59fb5f5b9ae119cf0c4048d4a9baf89e002b
KaiLegend56 <kaininja567@gmail.com>
2024-04-21 13:36:26 +0530

    made cart quantity interactive

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
@@ -80,11 +80,17 @@ document.querySelectorAll('.js-add-to-cart')
        
       );
       }
-        console.log(cart);
-      
-        
-    
-})
+           
+        let cartQuantity=0;
+        cart.forEach((item)=>
+          {
+            cartQuantity+=item.quantity;
+          }
+        )
+        document.querySelector('.cart-quantity')
+        .innerHTML=cartQuantity;
+        })
+
 })
 
 
