190629113fd7f53123a4c0aded844430f9d05cb0
KaiLegend56 <kaininja567@gmail.com>
2024-04-24 21:39:22 +0530

    copied whole code after checkout was not displaying and radio selector work

--- javascript-amazon-project-main/javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/cart.js
@@ -1,42 +1,64 @@
-export let cart=[{
-id:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-quantity:2
-},{
-  id:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-quantity:5
-}];
-export function addToCart(productId){
-    let matchingItem;
-         cart.forEach((cartItem)=>{
-        
-          if(cartItem.productId===productId) 
-          matchingItem=cartItem;
-  
-        });
-        if(matchingItem)
-        matchingItem.quantity++;
-        else{
-  
-              cart.push(
-            {
-               productId:productId,
-               quantity:1
-               
-            }
-              );
-           }
-          }
-  const newCart=[];        
- export function deleteProduct(productId){
-  cart.forEach((product)=>
-{
-  
-  if(productId!==product.id)
-  {newCart.push(product);
-   
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId:'1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId:'2'
+  }];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId:'1'
+    });
   }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
+function totalPrice(){
+  let productCost=0;
+  cart.forEach((product)=>{
+    totalCost+=product.priceCents*product.quantity;
+
   })
-console.log(newCart);
- }
+  document.querySelector('.cart-item-cost')
+  .innerHTML=productCost;
   
 
+}
