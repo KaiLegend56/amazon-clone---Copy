d735a8d7b48d382d18f2ba725621ffc9d64bb220
KaiLegend56 <kaininja567@gmail.com>
2024-06-23 15:56:15 +0530

    add backend practice file

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
@@ -2,3 +2,4 @@ import { orderSummaryRender } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 orderSummaryRender();
 renderPaymentSummary();
+import '../data/backendPractice.js';
