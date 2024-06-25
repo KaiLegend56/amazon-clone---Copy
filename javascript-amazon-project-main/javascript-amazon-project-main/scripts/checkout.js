3d8f7f425bc5254be69f848427e0ffd1dcc302eb
KaiLegend56 <kaininja567@gmail.com>
2024-06-25 20:56:54 +0530

    promise then workking

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { orderSummaryRender } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 new Promise((resolve)=>{
     loadProducts(()=>{
         orderSummaryRender();
@@ -9,7 +10,21 @@ new Promise((resolve)=>{
     
     resolve();
 }).then(()=>{
+    return new Promise(()=>{
+        loadCart(()=>{
+            resolve();
+        });
+    })
     
-})
+}).then(()=>{
+    orderSummaryRender();
+    renderPaymentSummary();
+});
+/*loadProducts(()=>{
+    loadCart(()=>{
+        orderSummaryRender();
+        renderPaymentSummary();
+    })//nesting of callback
 
+});*/
 
